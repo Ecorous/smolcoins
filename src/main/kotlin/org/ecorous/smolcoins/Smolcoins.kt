c85734c6bf20a1eb0ed0752f1f86d80baab31f25
lilly <mae@maestoso.online>
2023-08-08 01:48:47 -0500

    use qkl registry dsl and also potentially optimize smolcoinsToItems

--- src/main/kotlin/org/ecorous/smolcoins/Smolcoins.kt
+++ src/main/kotlin/org/ecorous/smolcoins/Smolcoins.kt
@@ -3,17 +3,17 @@ package org.ecorous.smolcoins
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
-import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 import net.minecraft.util.collection.DefaultedList
 import org.quiltmc.loader.api.ModContainer
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings
+import org.quiltmc.qkl.library.registry.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object Smolcoins : ModInitializer {
-    fun id(id: String): Identifier {
+    private fun id(id: String): Identifier {
         return Identifier("smolcoins", id)
     }
 
@@ -25,7 +25,7 @@ object Smolcoins : ModInitializer {
         Identifier("minecraft", "gold_ingot") to 5,
         Identifier("minecraft", "netherite_ingot") to 100,
     )
-    val SMOLCOIN_SETTINGS = QuiltItemSettings()
+    private val SMOLCOIN_SETTINGS = QuiltItemSettings()
     val LOGGER: Logger = LoggerFactory.getLogger("smolcoins")
 
     val smolcoin_1: Item = Item(SMOLCOIN_SETTINGS)
@@ -36,12 +36,14 @@ object Smolcoins : ModInitializer {
     val smolcoin_100: Item = Item(SMOLCOIN_SETTINGS)
 
     override fun onInitialize(mod: ModContainer) {
-        Registry.register(Registries.ITEM, id("smolcoin_1"), smolcoin_1)
-        Registry.register(Registries.ITEM, id("smolcoin_5"), smolcoin_5)
-        Registry.register(Registries.ITEM, id("smolcoin_10"), smolcoin_10)
-        Registry.register(Registries.ITEM, id("smolcoin_25"), smolcoin_25)
-        Registry.register(Registries.ITEM, id("smolcoin_50"), smolcoin_50)
-        Registry.register(Registries.ITEM, id("smolcoin_100"), smolcoin_100)
+        Registries.ITEM {
+            smolcoin_1 withId id("smolcoin_1")
+            smolcoin_5 withId id("smolcoin_5")
+            smolcoin_10 withId id("smolcoin_10")
+            smolcoin_25 withId id("smolcoin_25")
+            smolcoin_50 withId id("smolcoin_50")
+            smolcoin_100 withId id("smolcoin_100")
+        }
         LOGGER.info("Hello Quilt world from {}!", mod.metadata()?.name())
     }
 
@@ -49,30 +51,28 @@ object Smolcoins : ModInitializer {
         val items = mutableListOf<ItemStack>()
 
         var remainingSmolcoins = smolcoins
-        while (remainingSmolcoins >= 100) {
-            items.add(smolcoin_100.defaultStack)
-            remainingSmolcoins -= 100
-        }
-        while (remainingSmolcoins >= 50) {
-            items.add(smolcoin_50.defaultStack)
-            remainingSmolcoins -= 50
-        }
-        while (remainingSmolcoins >= 25) {
-            items.add(smolcoin_25.defaultStack)
-            remainingSmolcoins -= 25
-        }
-        while (remainingSmolcoins >= 10) {
-            items.add(smolcoin_10.defaultStack)
-            remainingSmolcoins -= 10
-        }
-        while (remainingSmolcoins >= 5) {
-            items.add(smolcoin_5.defaultStack)
-            remainingSmolcoins -= 5
-        }
-        while (remainingSmolcoins >= 1) {
-            items.add(smolcoin_1.defaultStack)
-            remainingSmolcoins -= 1
-        }
+
+        val smolcoins100 = remainingSmolcoins / 100
+        items.add(ItemStack(smolcoin_100, smolcoins100))
+        remainingSmolcoins -= smolcoins100 * 100
+
+        val smolcoins50 = remainingSmolcoins / 50
+        items.add(ItemStack(smolcoin_50, smolcoins50))
+        remainingSmolcoins -= smolcoins50 * 50
+
+        val smolcoins25 = remainingSmolcoins / 25
+        items.add(ItemStack(smolcoin_25, smolcoins25))
+        remainingSmolcoins -= smolcoins25 * 25
+
+        val smolcoins10 = remainingSmolcoins / 10
+        items.add(ItemStack(smolcoin_10, smolcoins10))
+        remainingSmolcoins -= smolcoins10 * 10
+
+        val smolcoins5 = remainingSmolcoins / 5
+        items.add(ItemStack(smolcoin_5, smolcoins5))
+        remainingSmolcoins -= smolcoins5 * 5
+
+        items.add(ItemStack(smolcoin_1, remainingSmolcoins))
 
         return items.groupBy { it.item }.map { (_, itemStacks) ->
             itemStacks.reduce { acc, itemStack -> acc.also { it.count += itemStack.count } }
